6d9da423b54079f0777d2f9a0b5eb6efc5c851ef
Tako_Seijin <TakoSeijin@users.noreply.github.com>
2015-10-09 18:25:03 -0700

    initial commit

---
+++ ui.js
@@ -0,0 +1,11 @@
+const CamWell = (
+  <Well>Look I'm in a well!
+  <li><iframe width="560" height="315" src="https://www.youtube.com/embed/wSKTPQDVGOc" frameborder="0" allowfullscreen></iframe></li>
+ 	<li><iframe width="560" height="315" src="https://www.youtube.com/embed/vJC62rvx6X4" frameborder="0" allowfullscreen></iframe></li>
+  <li><iframe width="560" height="315" src="https://www.youtube.com/embed/dzPOJkHwbjQ" frameborder="0" allowfullscreen></iframe></li>
+  <li><iframe width="560" height="315" src="https://www.youtube.com/embed/Pm94GM1rjeQ" frameborder="0" allowfullscreen></iframe></li>
+  </Well>
+
+);
+
+React.render(CamWell, document.body);
